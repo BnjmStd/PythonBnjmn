9e57116c0c7b82131de0eca0016c371f01b27561
Benjaaaamín <benja_e_e@hotmail.com>
2024-10-04 09:46:26 -0300

    add app1 and app2 python flask aplication

---
+++ flask/app2/models.py
@@ -0,0 +1,15 @@
+from config import db
+
+class Contact(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    firstName = db.Column(db.String(80), unique=False, nullable=False)
+    lastName = db.Column(db.String(80), unique=False, nullable=False)
+    email = db.Column(db.String(120), unique=False, nullable=False)
+
+    def to_json(self):
+        return {
+            "id": self.id,
+            "firstName": self.firstName,
+            "lastName": self.lastName,
+            "email": self.email
+        }
